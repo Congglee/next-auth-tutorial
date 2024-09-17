b0a7e4758ff1b0d8313f3eb28e74da9600de6a56
Congglee <congldqn888@gmail.com>
2024-09-18 05:47:25 +0700

    Middleware & Login

---
+++ auth.config.ts
@@ -0,0 +1,32 @@
+import bcrypt from "bcryptjs";
+import { getUserByEmail } from "@/data/user";
+import { LoginSchema } from "@/schemas";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const user = await getUserByEmail(email);
+
+          // If the user is not found in the database or the password is incorrect so everything can be ok to move on
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) {
+            return user;
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
---
+++ middleware.ts
@@ -0,0 +1,51 @@
+import authConfig from "@/auth.config";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
+import NextAuth, { Session } from "next-auth";
+import { NextRequest } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(
+  (req: NextRequest & { auth: Session | null }): Response | void => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    // Allow every single api route to be accessed
+    if (isApiAuthRoute) {
+      return;
+    }
+
+    if (isAuthRoute) {
+      // Redirect to the default login redirect if the user is already logged in
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      }
+      return;
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+      return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return;
+  }
+);
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
